c45555c45fb92fb0beadba9387b6d0d462a4f533
enteitsuki <enteitsuki@gmail.com>
2021-02-23 18:04:02 +0900

    for→foreachに変更 switch フラグ使用

--- index.php
+++ index.php
@@ -2,29 +2,34 @@
 $charges = [1000, 3000, 5000];
 
 echo "Suicaにチャージします。金額に該当する番号を入力してください。\n";
-for ($i = 0; $i < count($charges); $i++) {
-    $num = $i + 1;
-    echo $num . ': ' . number_format($charges[$i]) . "円\n";
+foreach ($charges as $charge) {
+    $num += 1;
+    echo $num . ': ' . number_format($charges[$num - 1]) . "円\n";
 }
 echo '番号: ';
 
 $answer = trim(fgets(STDIN));
-
-if (empty($answer) || $answer > count($charges)) {
-    echo "無効な番号です。処理を終了します。\n";
-    exit;
+$money = $charges[$answer - 1];
+switch ($answer) {
+    case 1:
+    case 2:
+    case 3:
+        echo $money . "円チャージしました。\n";
+        break;
+    default:
+        echo "無効な番号です。処理を終了します。\n";
+        exit;
 }
 
-$money = $charges[$answer - 1];
-echo $money . "チャージしました。\n";
-
-while ($money > 0) {
+$flag = true;
+while ($flag) {
     echo '商品の価格を入力して下さい:';
     $price = trim(fgets(STDIN));
-    $money -= $price;
-    if ($money > 0) {
-        echo '残高は' . $money . "です。\n";
+    if ($money >= $price) {
+        $money -= $price;
     } else {
-        echo "チャージ金額を上回るため購入できません。\n買い物を終了します。\n";
+        $flag = false;
     }
+    echo '残高は' . $money . "です。\n";
 }
+echo "チャージ金額を上回るため購入できません。\n買い物を終了します。\n";
